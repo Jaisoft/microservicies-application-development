0208ee12cde4909f45d1764e3832c4580ec55836
Jaime Gómez Moraleda <jaime@Jaimes-MacBook-Pro-2.local>
2024-02-26 00:33:09 +0100

    refactoring

--- frontend/languages/javascript/data-structure/modules/in-browser/modules-in-the-browser/app.js
+++
@@ -1,4 +0,0 @@
-import { tag } from './html.js'
-
-const h1 = tag('h1', '👋 Hello Modules!')
-document.body.appendChild(h1)
--- frontend/languages/javascript/flow-control/for/break.js
+++
@@ -1,7 +0,0 @@
-let text = "";
-for (let i = 0; i < 10; i++) {
-  if (i === 3) { break; }
-  text += "The number is " + i + "\n";
-}
-
-console.log(text)
